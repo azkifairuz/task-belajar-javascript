86309eba18648041181d98a7961440820172f811
Zidny-ilma <zidnyilmz@gmail.com>
2026-03-28 22:38:36 +0700

    update jawaban task 3

--- soal2.js
+++ soal2.js
@@ -14,7 +14,23 @@ const mahasiswa = {
 
 
 //1.Destructure nama, jurusan, dan ipk dalam 1 baris
+const {nama,jurusan,ipk} = mahasiswa
+console.log(nama,jurusan,ipk)
+
 //2.Destructure kota dari nested object alamat
+const {kota} = mahasiswa.alamat
+console.log(kota)
+
 //3.Destructure mata kuliah pertama dan kedua dari array mataKuliah
+const [m1,m2] = mahasiswa.mataKuliah
+console.log(m1,m2)
+
 //4.Buat function cetakProfil({ nama, jurusan, ipk }) yang menerima parameter destructured
+function cetakProfil({ nama, jurusan, ipk }) {
+  console.log("Nama:", nama),
+  console.log("Jurusan:", jurusan),
+  console.log("IPK:", ipk)
+}
+
 //5 Cetak semua hasilnya
+cetakProfil(mahasiswa)
